23e56311caeb191d8687d6bc498c7a6743387b00
Rowan-Nag <rowan_nag@icloud.com>
2023-07-12 19:36:51 -0400

    Squashed commit of the following:
    commit 8b62e23232a26cd20c63cacd0245882d817ddfd3
    Author: Rowan-Nag <rowan_nag@icloud.com>
    Date:   Wed Jul 12 19:35:48 2023 -0400
    
        quick mobile header fix
    
    commit 9bd8ae7eee767c83536a288f0fec50526e369439
    Author: Rowan-Nag <rowan_nag@icloud.com>
    Date:   Wed Jul 12 19:30:31 2023 -0400
    
        Update layout.js
    
    commit 90a2212ce21735c705b9164b09c6e99df5693f7c
    Merge: 1108dc7 1c267c2
    Author: Rowan Nag <rowan_nag@icloud.com>
    Date:   Wed Jul 12 11:26:47 2023 -0400
    
        Merge pull request #3 from Rowan-Nag/preview
    
        Finalized meta / most sections

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Page() {
 
   return (
     <main className="flex flex-col min-h-screen justify-center text-neutral-300 font-rubik bg-neutral-900 max-w-full">        
-    <header className='fixed text-neutral-300 top-0 flex flex-row justify-between bg-neutral-900 bg-opacity-90 w-full z-20 border-2 border-neutral-700 rounded-sm select-none'>
+    <header className='fixed text-neutral-300 top-0 flex flex-row justify-between bg-neutral-900 bg-opacity-90 w-full z-20 border-2 border-neutral-700 rounded-sm select-none sm:hidden'>
 
         <a className='w-full p-3 m-2 mx-8 text-center hover:bg-neutral-800 hover:text-neutral-200 rounded-md cursor-pointer transition-colors' href="#home">Home</a>
         <a className='w-full p-3 m-2 mx-8 text-center hover:bg-neutral-800 hover:text-neutral-200 rounded-md cursor-pointer transition-colors' href="#about">About</a>
