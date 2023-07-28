f53d31d2b21a224298e917a06b03871f01168576
Rowan-Nag <rowan_nag@icloud.com>
2023-07-28 14:21:14 -0400

    OG Desc Update
    I know no one looks at these but I NEED THEM TO BE PERFECT

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
   title: 'Rowan Nag | Student at Purdue University',
   description: "I'm Rowan Nag, a current undergraduate student at Purdue University studying Planetary Science and Physics. I do web-development, photography, and science-things.",
   openGraph :{
-    title: "Rowan Nag | Undergraduate",
+    title: "Rowan Nag | Undergraduate Student",
     description: "Planets • Physics • Programming"
 
   },
